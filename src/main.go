a73531f19bcd345d93617381c4e78247fb0e744d
keigo <k5.trismegistus@gmail.com>
2019-04-09 21:37:18 +0900

    :sparkles: app-init

--- src/main.go
+++ src/main.go
@@ -1,9 +1,29 @@
-package main
+package app
 
 import (
-	"fmt"
+	"github.com/tendermint/tendermint/libs/log"
+  "github.com/cosmos/cosmos-sdk/x/auth"
+
+  bam "github.com/cosmos/cosmos-sdk/baseapp"
+  dbm "github.com/tendermint/tendermint/libs/db"
 )
 
-func main() {
-	fmt.Println("hello, world")
+const (
+	appName = "nameservice"
+)
+
+type nameServiceApp struct {
+	*bam.BaseApp
+}
+
+func NewNameServiceApp(logger log.Logger, db dbm.DB) *nameServiceApp {
+	cdc := MakeCodec()
+	bApp := bam.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc))
+
+	var app = &nameServiceApp {
+		BaseApp: bApp,
+		cdc: cdc,
+	}
+
+	return app
 }
