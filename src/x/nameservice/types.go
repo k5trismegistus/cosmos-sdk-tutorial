5761ef0b2cdeef9c9614a562986ff004ad19691e
keigo <k5.trismegistus@gmail.com>
2019-04-09 22:04:03 +0900

    :sparkles: types

---
+++ src/x/nameservice/types.go
@@ -0,0 +1,22 @@
+package nameservice
+
+import (
+	"fmt"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type Whois struct {
+	Value string         `json:"value"`
+	Owner sdk.AccAddress `json:"owner"`
+	Price sdk.Coins      `json:"price"`
+}
+
+var MinNamePrice = sdk.Coins{sdk.NewInt64Coin("nametoken", 1)}
+
+func NewWhois() Whois {
+	return Whois {
+		Price: MinNamePrice
+	}
+}
